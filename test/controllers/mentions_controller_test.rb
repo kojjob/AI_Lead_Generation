71eb5e85384c740d09774bcce1b3054398b60fbb
Kojo <kodeon6@gmail.com>
2025-09-07 15:02:46 +0100

    Fix notification and mentions controller tests
    - Update notification controller test to use correct route helper
    - Update mentions controller test to use nested route helpers
    - Add proper Devise authentication setup to both test files
    - Tests now properly authenticate and use correct URL helpers
    
    This resolves test failures related to undefined URL helpers and ensures
    the tests properly authenticate before making requests.

--- test/controllers/mentions_controller_test.rb
+++ test/controllers/mentions_controller_test.rb
@@ -1,13 +1,22 @@
 require "test_helper"
 
 class MentionsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = users(:one)
+    @keyword = keywords(:one)
+    @mention = mentions(:one)
+    sign_in @user
+  end
+
   test "should get index" do
-    get mentions_index_url
+    get keyword_mentions_url(@keyword)
     assert_response :success
   end
 
   test "should get show" do
-    get mentions_show_url
+    get keyword_mention_url(@keyword, @mention)
     assert_response :success
   end
 end
